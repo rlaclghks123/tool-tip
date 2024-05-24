15f799da9544faa29c96a0f2ad2b8446ac1f971a
rlaclghks123 <rlaclghks123@naver.com>
2024-05-24 13:33:42 +0900

    feat: CustomColorToltips 컴포넌트 생성

---
+++ src/components/CustomColorTooltips/index.tsx
@@ -0,0 +1,81 @@
+import useHoverTooltip from '../../hooks/useHoverTooltip';
+import Tooltip from '../Tooltip.tsx';
+import './index.css';
+
+function PinkContents() {
+  return <div className="pink-contents-container">pink</div>;
+}
+function YellowContents() {
+  return <div className="yellow-contents-container">Yellow</div>;
+}
+
+function CustomColorTooltips() {
+  const {
+    isOpen: pinkIsOpen,
+    position: pinkPosition,
+    handleMouseOver: pinkHandleMouseOver,
+    handleMouseLeave: pinkHandleMouseLeave,
+  } = useHoverTooltip({
+    direction: 'top',
+  });
+
+  const {
+    isOpen: yellowIsOpen,
+    position: yellowPosition,
+    handleMouseOver: yellowHandleMouseOver,
+    handleMouseLeave: yellowHandleMouseLeave,
+  } = useHoverTooltip({
+    direction: 'top',
+  });
+
+  return (
+    <>
+      <section className="custom-content-tooltips-container custom-color-tooltips-container">
+        <button
+          className="tooltip-btn pink-button"
+          onMouseOver={(e) => pinkHandleMouseOver(e)}
+          onMouseLeave={() => pinkHandleMouseLeave(true)}
+        >
+          Pink
+        </button>
+        <button
+          className="tooltip-btn yellow-button"
+          onMouseOver={(e) => yellowHandleMouseOver(e)}
+          onMouseLeave={() => yellowHandleMouseLeave(true)}
+        >
+          Yellow
+        </button>
+      </section>
+
+      {pinkIsOpen && (
+        <Tooltip
+          backgroundColor="#fac"
+          borderColor="#fac"
+          position={{
+            top: pinkPosition.top + 5,
+            left: pinkPosition.left + 14,
+            direction: 'top',
+          }}
+        >
+          <PinkContents />
+        </Tooltip>
+      )}
+
+      {yellowIsOpen && (
+        <Tooltip
+          backgroundColor="#fff1aa"
+          borderColor="#fff1aa"
+          position={{
+            top: yellowPosition.top + 5,
+            left: yellowPosition.left + 4,
+            direction: 'top',
+          }}
+        >
+          <YellowContents />
+        </Tooltip>
+      )}
+    </>
+  );
+}
+
+export default CustomColorTooltips;
