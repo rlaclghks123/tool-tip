36dbde28e5e99b50713db358cb07cb4411a65ae9
rlaclghks123 <rlaclghks123@naver.com>
2024-05-26 20:30:18 +0900

    refactor: BasicTooltips 컴포넌트 왼쪽 button-container, 오른쪽 button-container 다르게 처리

--- src/components/BasicTooltips/index.tsx
+++ src/components/BasicTooltips/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 import './index.css';
 import Tooltip from '../Tooltip.tsx';
@@ -14,11 +14,32 @@ function BasicTooltips() {
     direction: '',
   });
 
-  const { isOpen, position, setPosition, handleMouseOver, handleMouseLeave } = useHoverTooltip({
+  const ref = useRef(null);
+
+  const {
+    isOpen: nonScrollIsOpen,
+    position: nonScrollPosition,
+    setPosition: nonScrollSetPosition,
+    handleMouseOver: nonScrollHandleMouseOver,
+    handleMouseLeave: nonScrollHandleMouseLeave,
+  } = useHoverTooltip({
     direction: '',
   });
 
-  function handleExpandMouseOver(e: React.MouseEvent<HTMLButtonElement>, info: BasicTooltipsType) {
+  const {
+    isOpen: scrollIsOpen,
+    position: scrollPosition,
+    setPosition: scrollSetPosition,
+    handleMouseOver: scrollHandleMouseOver,
+    handleMouseLeave: scrollHandleMouseLeave,
+  } = useHoverTooltip({
+    direction: '',
+  });
+
+  function nonScrollHandleExpandMouseOver(
+    e: React.MouseEvent<HTMLButtonElement>,
+    info: BasicTooltipsType
+  ) {
     const { direction, tooltipContents } = info;
     const position = {
       top: { changeTop: 30, changeLeft: 10 },
@@ -29,11 +50,39 @@ function BasicTooltips() {
 
     const { changedTop, changedLeft } = changedPosition({ direction, position });
 
-    handleMouseOver(e);
+    nonScrollHandleMouseOver(e);
 
-    setPosition((prev) => ({
+    nonScrollSetPosition((prev) => ({
       left: prev.left - changedLeft,
       top: prev.top - changedTop,
+      direction,
+    }));
+
+    setTooltipState(() => {
+      return {
+        contents: tooltipContents,
+        direction: direction,
+      };
+    });
+  }
+
+  function handleExpandMouseOver(e: React.MouseEvent<HTMLButtonElement>, info: BasicTooltipsType) {
+    const { top, left } = e.currentTarget.getBoundingClientRect();
+    const { direction, tooltipContents } = info;
+    const position = {
+      top: { changeTop: 30, changeLeft: 13 },
+      left: { changeTop: -30, changeLeft: 110 },
+      right: { changeTop: -30, changeLeft: -80 },
+      bottom: { changeTop: -90, changeLeft: 15 },
+    };
+
+    const { changedTop, changedLeft } = changedPosition({ direction, position });
+
+    scrollHandleMouseOver(e);
+
+    scrollSetPosition((prev) => ({
+      left: left - changedLeft,
+      top: top - changedTop,
       direction,
     }));
 
@@ -50,21 +99,31 @@ function BasicTooltips() {
       <div className="basic-tooltips-wrapper">
         <div className="non-scroll-container">
           <CommonBasicTooltipsContainer
-            handleMouseLeave={handleMouseLeave}
-            handleExpandMouseOver={handleExpandMouseOver}
+            handleMouseLeave={nonScrollHandleMouseLeave}
+            handleExpandMouseOver={nonScrollHandleExpandMouseOver}
           />
         </div>
 
-        <div className="scroll-box">
+        <div className="scroll-box" ref={ref}>
           <CommonBasicTooltipsContainer
-            handleMouseLeave={handleMouseLeave}
+            handleMouseLeave={scrollHandleMouseLeave}
             handleExpandMouseOver={handleExpandMouseOver}
           />
         </div>
       </div>
 
-      {isOpen && (
-        <Tooltip position={position}>
+      {nonScrollIsOpen && (
+        <Tooltip position={nonScrollPosition}>
+          <div className="default-tooltip-contents">
+            {tooltipState?.contents.map((content, idx) => (
+              <p key={idx}>{content}</p>
+            ))}
+          </div>
+        </Tooltip>
+      )}
+
+      {scrollIsOpen && (
+        <Tooltip position={scrollPosition}>
           <div className="default-tooltip-contents">
             {tooltipState?.contents.map((content, idx) => (
               <p key={idx}>{content}</p>
